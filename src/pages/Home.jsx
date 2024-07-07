a6b779066ee957708cb8739aadbedd87256c028e
Siddharth Joisar <sidjoisar0807@gmail.com>
2024-07-07 21:15:19 +0530

    .env change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
         <Link to={"/signup"}>
           <div className="group mx-auto mt-16 w-fit rounded-full bg-richblack-800 p-1 font-bold text-richblack-200 drop-shadow-[0_1.5px_rgba(255,255,255,0.25)] transition-all duration-200 hover:scale-95 hover:drop-shadow-none">
             <div className="flex flex-row items-center gap-2 rounded-full px-10 py-[5px] transition-all duration-200 group-hover:bg-richblack-900">
-              <p>Become an Instructor</p>
+              <p className='cursor-pointer'>Become an Instructor</p>
               <FaArrowRight />
             </div>
           </div>
